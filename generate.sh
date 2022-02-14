e7391d2c175630d2a6bb324ef23626b735781612
Artur Sobczyk <artur.sobczyk83@outlook.com>
2022-02-14 15:34:54 +0100

    template file to use in ec2

---
+++ generate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+export INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
+export PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
+export PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+export MAC=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs`
+export AZ=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`
+export AZ_ID=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone-id`
+export SUBNET=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/subnet-id`
+
+export VERSION="1.0.0"
+export DATE=`date --utc +%FT%T%Z`
+
+envsubst < template.html | tee index.html
+
